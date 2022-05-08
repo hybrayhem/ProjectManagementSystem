c4a75a7e871a24be8d27cf4c61f117f701bd6813
Halil <halil000ibrahim@gmail.com>
2022-05-08 17:01:09 +0300

    implement Binary Search Tree

---
+++ src/BST/BinarySearchTree.java
@@ -0,0 +1,26 @@
+package BST;
+
+public class BinarySearchTree<E extends Comparable<E>>
+        extends BinaryTree<E>
+        implements SearchTree<E> {
+
+    public BinarySearchTree() {
+        // TODO: will be implemented
+    }
+
+    public boolean add(E item) {
+        return false;
+    }
+
+    public boolean contains(E target) {
+        return false;
+    }
+
+    public boolean remove(E target) {
+        return false;
+    }
+
+    public E search(E target){
+        return null;
+    }
+}
