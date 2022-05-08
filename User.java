83d34c7fc882f3d45da37dfc4415b39e12861c60
Yusuf Alperen Dönmez <49916000+DaPhantomxx@users.noreply.github.com>
2022-05-08 22:39:07 +0300

    Add files via upload

---
+++ User.java
@@ -0,0 +1,46 @@
+/** User of the System. */
+public abstract class User {
+    // Data Fields
+    /**  */
+    private int id;
+    /**   */
+    private String username;
+    /**  */
+    private String fullname;
+    /**  */
+    private int contact;
+    /**  */
+    private String teams;
+
+    // Constructors
+    public User(int id, String username, String fullname, int contact, String teams) {
+        this.id = id;
+        this.username = username;
+        this.fullname = fullname;
+        this.contact = contact;
+        this.teams = teams;
+    }
+
+    public User(User guest) {
+
+    }
+
+    // Methods
+    public abstract void viewBoard(Board board);
+    
+    public abstract status changeIssueStatus(Issue issue, Status newStatus) {
+
+    }
+
+    public abstract void editIssueTitle(Issue issue, String newTitle) {
+
+    }
+
+    public abstract void removeIssueComment(Issue issue) {
+
+    }
+
+    public abstract void addIssueComment(Issue issue, Comment newComment) {
+
+    }
+}
