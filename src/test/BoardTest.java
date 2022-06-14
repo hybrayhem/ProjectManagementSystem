4e459a3c9fcc9527dd5774c24fdd41ffc354bbda
Halil <halil000ibrahim@gmail.com>
2022-06-14 17:27:36 +0300

    add boardTest, backlogTest

---
+++ src/test/BoardTest.java
@@ -0,0 +1,10 @@
+package test;
+
+import ProjectManagementSystem.Board;
+
+public class BoardTest {
+    public static void main(String[] args) {
+        System.out.println("*** Test Board ***");
+        Board board = new Board();
+    }
+}
